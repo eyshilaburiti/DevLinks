de1cee363b3c22a8eee12933f78c3927da4d0de2
buritieyshila <buritieyshila@gmail.com>
2024-01-16 13:46:31 -0300

    atualizando atributos html com JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function toggleMode() {
+  // substituição do background
   const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const image = document.querySelector("#profile img") // pesquisa pelo seletor css
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    image.setAttribute("src", "./assets/avatar-light.png") // adicionar/modificar algum atributo
+  } else {
+    // se tiver sem light mode, adicionar a imagem normal
+    image.setAttribute("src", "./assets/avatar.png")
+  }
 
   /*
   if (html.classList.contains("light")) {
@@ -8,5 +22,4 @@ function toggleMode() {
     html.classList.add("light")
   }
   */
-  html.classList.toggle("light")
 }
